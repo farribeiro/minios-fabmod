72473a4dc5ea2ccbbe7804f286a50817e0b8f5f3
Fabio Rodrigues Ribeiro <farribeiro@gmail.com>
2023-12-29 12:12:06 -0300

    Change fabio to flatpak.sh and split some commands
    # Conflicts:
    #	flatpak.sh

--- flatpak.sh
+++ flatpak.sh
@@ -2,6 +2,3 @@
 apt update
 apt install flatpak -y
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-flatpak install flathub com.microsoft.Edge -y
-flatpak install flathub org.telegram.desktop -y
-
